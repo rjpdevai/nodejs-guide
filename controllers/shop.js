ed6b11cad813fd6d4eb7b11b3c308e2ca8e58408
Rjpdevai <rjpdevai@gmail.com>
2025-10-05 02:16:29 -0400

    delete cart added

--- controllers/shop.js
+++ controllers/shop.js
@@ -21,7 +21,18 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', { pageTitle: 'Your Cart', path: '/cart' });
+    Cart.getProducts(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for (product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if (cartProductData) {
+                    cartProducts.push({ productData: product, qty: cartProductData.qty });
+                }
+            }
+            res.render('shop/cart', { pageTitle: 'Your Cart', path: '/cart', products: cartProducts, totalPrice: cart.totalPrice });
+        });
+    });
 };
 
 exports.postCart = (req, res, next) => {
@@ -34,6 +45,14 @@ exports.postCart = (req, res, next) => {
 
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', { pageTitle: 'Checkout', path: '/checkout' });
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+        Cart.deleteProduct(prodId, product.price);
+        res.redirect('/cart');
+    });
 };
 
 exports.getOrders = (req, res, next) => {
