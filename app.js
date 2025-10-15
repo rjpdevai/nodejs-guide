6342c69b2d64f88fa7c47fd689b4e1a0773fe5e2
Rjpdevai <rjpdevai@gmail.com>
2025-10-15 03:20:48 -0400

    login, signup, authentication and code update

--- app.js
+++ app.js
@@ -45,18 +45,18 @@ app.use(errorController.get404);
 
 mongoose.connect(MONGODB_URI)
     .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'Rit',
-                    email: 'rjp@test.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        });
+        // User.findOne().then(user => {
+        //     if (!user) {
+        //         const user = new User({
+        //             name: 'Rit',
+        //             email: 'rjp@test.com',
+        //             cart: {
+        //                 items: []
+        //             }
+        //         });
+        //         user.save();
+        //     }
+        // });
         app.listen(3000);
     }).catch(err => {
         console.log(err);
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
-router.get('/admin/add-product', adminController.getAddProduct);
+router.get('/admin/add-product', isAuth, adminController.getAddProduct);
 
-router.post('/admin/add-product', adminController.postAddProduct);
+router.post('/admin/add-product', isAuth, adminController.postAddProduct);
 
-router.get('/admin/products', adminController.getProducts);
+router.get('/admin/products', isAuth, adminController.getProducts);
 
-router.get('/admin/edit-product/:productId', adminController.getEditProduct);
+router.get('/admin/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/admin/edit-product', adminController.postEditProduct);
+router.post('/admin/edit-product', isAuth, adminController.postEditProduct);
 
-router.post('/admin/delete-product', adminController.postDeleteProduct);
+router.post('/admin/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
