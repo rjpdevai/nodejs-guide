0510005bd511ffb82b6a73456cdd80b8bb4024b3
Rjpdevai <rjpdevai@gmail.com>
2025-10-21 17:52:22 -0400

    Download pdf invoice authentication

--- routes/shop.js
+++ routes/shop.js
@@ -21,4 +21,6 @@ router.get('/checkout', isAuth, shopController.getCheckout);
 
 router.post('/create-order', isAuth, shopController.postOrder);
 
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
+
 module.exports = router;
